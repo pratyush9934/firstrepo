a23e85a8ea850a2587e84dfc6bcb065a50d5c054
pratyush9934 <86598933+pratyush9934@users.noreply.github.com>
2021-06-28 15:57:12 +0530

    fist commite
    vgfhgfy

---
+++ Models/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmDemo.Models
+{
+    public class Employee : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private void onPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { id = value; onPropertyChanged("Id"); }
+        }
+        private string name;
+        public string Name
+        {
+        get{ return name; }
+         set{ name = value; onPropertyChanged("Name"); }
+
+        }
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; onPropertyChanged("Age"); }
+
+        }
+            
+
+            
+        
+    }
+}
+
---
+++ Models/EmployeeService.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmDemo.Models
+{
+   public class EmployeeService
+    {
+        private static List<Employee> ObjEmployeesList;
+        public EmployeeService()
+        {
+            ObjEmployeesList = new List<Employee>()
+            {
+                new Employee{Id=101,Name="Syed",Age=25}
+            };
+        }
+        public List<Employee>GetAll()
+        {
+            return ObjEmployeesList;
+        }
+        public bool Add(Employee objEmployee)
+        {
+            //Age must be between 21 and 58
+            if (objEmployee.Age < 21 || objEmployee.Age > 58)
+                throw new ArgumentException("Invalid age limit for employee");
+            ObjEmployeesList.Add(objEmployee);
+                return true;
+        }
+        public bool Update(Employee objEmployeeToUpdate)
+
+        {
+            bool IsUpdated = false;
+            for (int index=0;index<ObjEmployeesList.Count;index++)
+            {
+                if(ObjEmployeesList[index].Id==objEmployeeToUpdate.Id)
+                {
+                    ObjEmployeesList[index].Name = objEmployeeToUpdate.Name;
+                    ObjEmployeesList[index].Age = objEmployeeToUpdate.Age;
+                    IsUpdated = true;
+                    break;
+                }
+            }
+                return IsUpdated;
+        }
+        public bool Delete(int id)
+        {
+            bool IsDeleted = false;
+            for (int index = 0; index < ObjEmployeesList.Count; index++ )
+            {
+                if (ObjEmployeesList[index].Id == id)
+                {
+                    ObjEmployeesList.RemoveAt(index);
+                    IsDeleted = true;
+                    break;
+                }
+                        
+            }
+            return IsDeleted;
+
+        }
+        public Employee Search(int id)
+        {
+            return ObjEmployeesList.FirstOrDefault(e => e.Id == id);
+        }
+    }
+}
